f695ef38ab4e4dd73734e434edcbb9bd615e9ef7
putubhanuartha <putuaryabhanu03@gmail.com>
2023-11-09 13:45:32 +0700

    test pull request

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -1,13 +1,16 @@
 package com;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        int x = userInput.nextInt();
+        System.out.println("Hello World! " + x);
+        userInput.close();
     }
 }
